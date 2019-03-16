c9d5f465b69d6df2774111ec21ce47eb8b16c51c
Josh Roy <joshnroy@gmail.com>
2019-03-16 11:37:12 -0400

    added zshrc and modified install to support it

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 git pull;
 rm $HOME/.config/nvim/init.vim
 rm $HOME/.tmux.conf
+rm $HOME/.zshrc
 # ln -s $HOME/dotfiles/vimrc $HOME/.vimrc;
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/dotfiles/vimrc $HOME/.config/nvim/init.vim;
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf;
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc;
 echo "linked files";
