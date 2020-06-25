e3efc22456bce6b3fb5a4085417bfff7c6365ca5
Josh Roy <joshnroy@gmail.com>
2020-06-25 15:53:55 -0400

    fixed install script

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 git pull;
-rm $HOME/.config/nvim/init.vim
-rm $HOME/.tmux.conf
-rm $HOME/.zshrc
+rm $HOME/.config/nvim/init.vim;
+rm $HOME/.tmux.conf;
 # ln -s $HOME/dotfiles/vimrc $HOME/.vimrc;
-mkdir -p $HOME/.config/nvim
-ln -s $HOME/dotfiles/vimrc $HOME/.config/nvim/init.vim;
+mkdir -p $HOME/.config/nvim;
+ln -s $HOME/dotfiles/vimrc $HOME/.vimrc;
 ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf;
-ln -s $HOME/dotfiles/zshrc $HOME/.zshrc;
 echo "linked files";
